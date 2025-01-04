895222d5b044ea09581c472b7be667f469453f6b
saparov7up <saparovcoding4@gmail.com>
2025-01-04 15:36:13 +0500

    True

---
+++ 2.2dars/2.2dars/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _2._2dars;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        if(4 == 9)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+
+    }
+}
